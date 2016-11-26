c97719cd20a4941b03686f78e40b71d760249b58
Beldiman Iuliana <b_iuliana.m200@yahoo.com>
2016-11-26 10:37:58 +0200

    Iuliana test commit

--- recruiting-Client/src/java/recruitingclient/Main.java
+++ recruiting-Client/src/java/recruitingclient/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         request.createTest("1", "Test 1");
         //FLORIN TEST COMMIT GIT
+        //IULIANA TEST COMMIT GIT
     }
     
 }
