5decdb6ee00fee73e137d022b1639ee20f6ae464
florindragoi95 <florindragoi95@gmail.com>
2016-11-26 18:46:16 +0200

    add candidate and job classes

---
+++ recruiting-EJB/src/java/recruiting/entity/Candidate.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package recruiting.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.ejb.EJB;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Flo
+ */
+
+@Entity
+@Table(name = "EJB_Candidate")
+public class Candidate implements Serializable{
+
+    @Id
+    @EJB
+    private int id;
+    private String first_name;
+    private String last_name;
+    private int id_job;
+
+    public Candidate(int id, String first_name, String last_name, int id_job) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.id_job = id_job;
+    }
+    
+    public Candidate(){}
+  
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public int getId_job() {
+        return id_job;
+    }
+
+    public void setId_job(int id_job) {
+        this.id_job = id_job;
+    }
+    
+    @ManyToMany(mappedBy = "candidates")
+    private Collection<Job> jobs;
+    
+    
+    
+    
+    
+}
