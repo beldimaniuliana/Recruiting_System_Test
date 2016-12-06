d8d97499dddc5d7caf3e268feb5f417f19d2b1ba
Beldiman Iuliana <b_iuliana.m200@yahoo.com>
2016-12-06 21:16:15 +0200

    Create and test 2 tables (person and person details) - trying to create connection between them but when I insert in the first table the foreign key from the second one is not visible in the first one.So the connection (one to one) is not working well.

--- recruiting-Client/src/java/recruitingclient/Main.java
+++ recruiting-Client/src/java/recruitingclient/Main.java
@@ -9,26 +9,35 @@ import recruiting.request.Request;
 public class Main {
     @EJB
     private static Request request;
-    private static Request user;
+    //private static Request user;
 
     public static void main(String[] args) {
+        //request.createTest2(6, "Test1");
+        //request.createTest("100", "Mama",4);
+        
+        request.createPerson(6, "Iuliana");
+        request.createPersonDetails(1, "303030", "Engeneer", 123);
+        //request.createPersonWithDetails(2, "Eu", 2, "deas", "adas", 252);
+                
+        
+        
+        
       //Functionale
-            // insert tabele job, candidate + insert in tabela de legatura
+        //insert tabele job, candidate + insert in tabela de legatura
         //request.createCandidate(1, "first_name", "last_name");
         //request.createJob(1, "position", "requirements", "locuri", "status", "data");
         //request.addCandidate(1, 1);
-            // delete din tabelele job, candidate + tabela de legatura
+        // delete din tabelele job, candidate + tabela de legatura
         //request.removeCandidate(1);
         //request.removeJob(1);
-        
-        request.createGroup(1, "name");
-        request.addPrivilage(1, 1);
+        //request.createGroup(1, "name");
+        //request.addPrivilage(1, 1);
         
         
      //Nefunctionale   
-      //  request.createTest("2", "Test 2");  
-      //  user.createUser(1, "iuli_200", "pass", "Beldiman", "Iuliana", 1, "iuliana@email.com", "yutyu", "true");
-      // request.createPrivilage(1, "adauga", "adaugare utilizator");
+        // request.createTest("2", "Test 2");  
+        // user.createUser(1, "iuli_200", "pass", "Beldiman", "Iuliana", 1, "iuliana@email.com", "yutyu", "true");
+        // request.createPrivilage(1, "adauga", "adaugare utilizator");
        
     }
     
